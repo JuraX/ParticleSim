cdd183d30786bc72ba51b0e05869fa9e6f80b2c9
TurnTheTideTM <richard.viehoever@yahoo.de>
2013-09-10 19:24:22 +0200

    KONSTANTEN in MainTest

--- src/MainTest.py
+++ src/MainTest.py
@@ -4,12 +4,18 @@ Created on 10.09.2013
 @author: RV Administrator
 '''
 
-import Particle, random
+import Particle
+from random import  *
+
+MASS_MIN = 1
+MASS_MAX = 10
+COORDS_MIN = -5
+COORDS_MAX = 5
 
 if __name__ == '__main__':
     particles = []
     for i in range(5):
-        particles.append(Particle.Particle(random.randint(1, 10), random.random() * 10 - 5, random.random() * 10 - 5))
+        particles.append(Particle.Particle(random()*(MASS_MAX - MASS_MIN) + MASS_MIN, random()*(COORDS_MAX - COORDS_MIN) + COORDS_MIN, random()*(COORDS_MAX - COORDS_MIN) + COORDS_MIN))
     for i in xrange(100):
         for j in particles:
             j.calcMovement(particles, 0.01)
